d5be0ee6624843cced3e8fb8c98674432b38321b
AthulJp07 <athuljp77@gmail.com>
2025-12-01 23:28:23 +0530

    added pass_count

--- score_analyzer.py
+++ score_analyzer.py
@@ -7,6 +7,8 @@ for i in range(n):
 avg = sum(score)/n
 max_score = max(score)
 min_score = min(score)
+pass_count = len([s for s in score if s>=40])
 print(f'The average score is {avg}')
 print(f'The highest score is {max_score}')
 print(f'The lowest score is {min_score}')
+print(f'The number of students who passed is {pass_count}')
