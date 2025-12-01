533983f8e4932f813c6d6fdee0313799569d8896
AthulJp07 <athuljp77@gmail.com>
2025-12-01 23:23:18 +0530

    add new prgm

---
+++ score_analyzer.py
@@ -0,0 +1,12 @@
+print('Enter the no of students:')
+n=int(input())
+score =[]
+for i in range(n):
+    score.append(int(input(f"Enter the score of student {i+1}\n")))
+
+avg = sum(score)/n
+max_score = max(score)
+min_score = min(score)
+print(f'The average score is {avg}')
+print(f'The highest score is {max_score}')
+print(f'The lowest score is {min_score}')
